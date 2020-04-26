28f3d487eba38c617a41bddae0f2d14f521a037a
Aleksandr Fomin <ll.wg.bin@gmail.com>
2020-04-26 20:51:29 +0200

    new -> cast

--- lib/extensions/numeric.ex
+++ lib/extensions/numeric.ex
@@ -3,6 +3,6 @@ defmodule Jamdb.Oracle.Extensions.Numeric do
   def decode(rows), do: rows
 
   defp decode_number({value}), do: decode_number(value)
-  defp decode_number(value) when is_number(value), do: Decimal.new(value)
+  defp decode_number(value) when is_number(value), do: Decimal.cast(value)
   defp decode_number(value), do: value
 end
